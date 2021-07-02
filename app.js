a7bf610aaa370f6240dfdbac067a36fcae08ece0
sumitrajpal29 <sumitrajpal29@users.noreply.github.com>
2021-07-02 19:54:48 +0530

    New SecretProject, added level 2 security, used mongoose-encryption.

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption")
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+
+const userSchema = new mongoose.Schema({
+  email:String,
+  password:String
+});
+const secret = "Thisissecretkeyforlesssecureencryption"
+userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+const User = new mongoose.model("User",userSchema);
+
+
+
+app.listen(3000,function(){
+  console.log("app is running on port 3000.");
+});
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+
+app.post("/register",function(req,res){
+  const newUser = new User({email:req.body.username,password:req.body.password})
+  newUser.save(function(err){
+    if(err) console.log(err);
+    else res.render("secrets");
+  })
+});
+
+app.post("/login",function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({email:username},function(err,found){
+    if(!err){
+      if(found){
+        res.render("secrets");
+      }
+      else console.log("Username not found!");
+    }
+    else console.log(err);
+  })
+});
