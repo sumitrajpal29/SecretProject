a6dbd27dcd94db4de0eb7f944039a517b96d0476
sumitrajpal29 <sumitrajpal29@users.noreply.github.com>
2021-07-03 00:52:38 +0530

    Added .env file.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -10,16 +11,14 @@ app.use(express.static("public"));
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
-
-
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
 
 const userSchema = new mongoose.Schema({
   email:String,
   password:String
 });
-const secret = "Thisissecretkeyforlesssecureencryption"
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 const User = new mongoose.model("User",userSchema);
 
 
