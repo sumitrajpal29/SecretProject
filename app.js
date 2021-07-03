77b090b0d99d5b81f891aab2f6e1a2ace2a09957
sumitrajpal29 <sumitrajpal29@users.noreply.github.com>
2021-07-03 19:22:43 +0530

    Security level 4, used Bcrypt to hash with 10 salting rounds.

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
-const md5 = require("md5");
+// const encrypt = require("mongoose-encryption")
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -42,22 +44,25 @@ app.get("/register",function(req,res){
 
 
 app.post("/register",function(req,res){
-  const newUser = new User({email:req.body.username,password:md5(req.body.password)})
-  newUser.save(function(err){
-    if(err) console.log(err);
-    else res.render("secrets");
-  })
+  bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+    const newUser = new User({email:req.body.username, password:hash});
+    newUser.save(function(err){
+      if(err) console.log(err);
+      else res.render("secrets");
+    })
+  });
 });
 
 app.post("/login",function(req,res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
   User.findOne({email:username},function(err,found){
     if(!err){
       if(found){
-        if(password===found.password)
-        res.render("secrets");
-        else res.send("Wrong password");
+        bcrypt.compare(password,found.password,function(err,result){
+          if(result) res.render("secrets");
+          else res.send("Wrong password");
+        })
       }
       else res.send("Username not found!");
     }
